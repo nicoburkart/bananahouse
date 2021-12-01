13fed9db6f68cbf4354cb7535a34d7ea001e40b8
Nico Burkart <nico@MacBook-Air-von-Nico.local>
2021-12-01 17:29:51 +0000

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Hero from 'components/hero/hero';
+import './styles/main.scss';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="header">Header</h1>
+      <h2 className="section-header">section</h2>
+      <h3 className="sub-section-header">sub-section</h3>
+      <p>hello world</p>
+      <a
+        className="sub-section-header"
+        style={{ textDecoration: 'underline', color: 'blue' }}
+        target="_blank"
+        href="https://www.figma.com/file/8wZT4wM6X43A0SZ00pDlHl/bananahouse?node-id=0%3A1"
+      >
+        open design file
+      </a>
+      <Hero></Hero>
     </div>
   );
 }
---
+++ src/components/hero/hero.tsx
@@ -0,0 +1,7 @@
+import './hero.scss';
+
+function Hero() {
+  return <div id="Hero"></div>;
+}
+
+export default Hero;
