ff1b803b9b88fa919320d9181db501fe0590e2f8
Nico Burkart <nico@MacBook-Air-von-Nico.local>
2021-12-02 15:42:13 +0000

    hero html

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,15 @@ import './styles/main.scss';
 function App() {
   return (
     <div className="App">
-      <h1 className="header">Header</h1>
-      <h2 className="section-header">section</h2>
-      <h3 className="sub-section-header">sub-section</h3>
-      <p>hello world</p>
-      <a
+      {/* <a
         className="sub-section-header"
         style={{ textDecoration: 'underline', color: 'blue' }}
         target="_blank"
         href="https://www.figma.com/file/8wZT4wM6X43A0SZ00pDlHl/bananahouse?node-id=0%3A1"
+        rel="noreferrer"
       >
         open design file
-      </a>
+      </a> */}
       <Hero></Hero>
     </div>
   );
--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -1,7 +1,30 @@
 import './hero.scss';
+import postcard from 'assets/images/postcard.png';
 
 function Hero() {
-  return <div id="Hero"></div>;
+  function openMail() {
+    window.open(
+      'mailto:merle@web.de?subject=booking%20request&body=add%20name%20and%20month'
+    );
+  }
+
+  return (
+    <div id="hero" className="container">
+      <div className="col-left">
+        <h1 className="header">The Banana House</h1>
+        <h2 className="section-header">
+          Join the banana crew <br />
+          in Ponta do Sol!
+        </h2>
+        <button onClick={openMail} className="primary-btn">
+          Contact
+        </button>
+      </div>
+      <div className="col-right">
+        <img src={postcard} alt="" />
+      </div>
+    </div>
+  );
 }
 
 export default Hero;
