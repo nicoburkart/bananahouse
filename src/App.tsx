a751dc6802fdba0a5f0a5f3d6e5f1b7d441ddd20
Nico Burkart <nico@Air-von-Nico.home>
2021-12-03 15:20:45 +0000

    removed comment

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,6 @@ import './styles/main.scss';
 function App() {
   return (
     <div className="App">
-      {/* <a
-        className="sub-section-header"
-        style={{ textDecoration: 'underline', color: 'blue' }}
-        target="_blank"
-        href="https://www.figma.com/file/8wZT4wM6X43A0SZ00pDlHl/bananahouse?node-id=0%3A1"
-        rel="noreferrer"
-      >
-        open design file
-      </a> */}
       <Hero></Hero>
     </div>
   );
